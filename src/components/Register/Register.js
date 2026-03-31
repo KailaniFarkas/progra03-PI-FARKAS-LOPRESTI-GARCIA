00a6014cf1f3f3aeb6e6562c9f6f1e29e2a1e1b3
Ornela <ornela@MacBook-Pro-de-Ornela.local>
2026-03-31 19:27:27 -0300

    register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Register() {
-  return (
-    <div>
-      <h2 className="alert alert-primary">Registro</h2>
-      <p>Acá va el formulario de registro</p>
-    </div>
-  );
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  actualizarDatos(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  evitarSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.email, this.state.password);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="alert alert-primary">Crear Cuenta</h2>
+        <form className="register-form" onSubmit={(event) => this.evitarSubmit(event)}>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={(event) => this.actualizarDatos(event)}
+          />
+          <label>Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={(event) => this.actualizarDatos(event)}
+          />
+          <input type="submit" value="Crear Cuenta" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Register;
