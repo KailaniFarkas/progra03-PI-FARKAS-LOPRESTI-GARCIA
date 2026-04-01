c60990d3f61a815e222c553c758e3cc373fcd5bc
mgarciapoitevin <mgarciapoitevin@udesa.edu.ar>
2026-04-01 16:06:43 -0300

    Screens creados e importados a App.js.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './styles.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './components/Home/Home';
-import Movies from './components/Movies/Movies';
-import Series from './components/Series/Series';
-import Favorites from './components/Favorites/Favorites';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-
+import React from "react";
+import "./styles.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./screens/Home/Home";
+import Movies from "./screens/Movies/Movies";
+import Series from "./screens/Series/Series";
+import Favorites from "./screens/Favorites/Favorites";
+import Login from "./screens/Login/Login";
+import Register from "./screens/Register/Register";
+import Results from "./screens/Results/Results";
+import DetailMovie from "./screens/DetailMovie/DetailMovie";
+import DetailSeries from "./screens/DetailSeries/DetailSeries";
+import NotFound from "./screens/NotFound/NotFound";
 
 function App() {
   return (
@@ -17,12 +20,15 @@ function App() {
       <div className="container">
         <Header />
         <Switch>
+          <Route path="/" component={Home} />
           <Route path="/movies" component={Movies} />
           <Route path="/series" component={Series} />
+          <Route path="/detailmovie/:id" component={DetailMovie} />
+          <Route path="/detailseries/:id" component={DetailSeries} />
           <Route path="/favorites" component={Favorites} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-          <Route path="/" component={Home} />
+          <Route path="/results" component={Results} />
           {/* <Route component={NotFound} /> */}
         </Switch>
         <Footer />
